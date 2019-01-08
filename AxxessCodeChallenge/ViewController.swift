f92a6caea07dce015aaad73e3025b56b8f6fcb46
Jean Pierre <jfigaredomoro@fullsail.edu>
2019-01-08 15:09:43 -0400

    first commit

---
+++ AxxessCodeChallenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AxxessCodeChallenge
+//
+//  Created by Jean Pierre on 1/8/19.
+//  Copyright © 2019 Jean Pierre. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
